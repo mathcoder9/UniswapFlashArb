8be2a1284c545a549560490ad8d41811b0b26633
Sayon <sayon_u@hotmail.co.uk>
2024-06-30 13:58:07 +0100

    initial contracts commit

---
+++ contracts/helper/Address.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library Address {
+    // Tokens
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant DAI = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    // Addresses
+    address public constant UNIV3_DAI_WETH_POOL =
+        0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
+    address public constant UNIV3_FACTORY =
+        0x1F98431c8aD98523631AE4a59f267346ea31F984;
+    address public constant UNIV2_DAI_WETH_POOL =
+        0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc;
+    address public constant UNIV2_ROUTER =
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+}
